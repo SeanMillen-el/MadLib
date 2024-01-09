b83757f6ce9c0c33c883293abc1e1643323046c5
Sean Millen-el <sean.millenel1907@gmail.com>
2024-01-09 16:21:54 -0500

    done

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let adjective = prompt("Enter an adjective:");
 let noun = prompt("Enter a noun:");
 let verb = prompt("Enter a verb:");
 let adverb = prompt("Enter an adverb:");
+let color = prompt("Enter a color:");
+let character = prompt("Enter a name")
 
-let story = `Once upon a time, during a ${adjective} and ${color} afternoon, I ${adverb} went on a ${adjective} journey to the theaters to watch the latest Barbie movie, titled "Barbie's ${adjective} ${noun}." Excitement bubbled within me as I ${verb} into my ${adjective} seat, surrounded by a ${noun} of ${verb-ing} ${noun} adorned in ${noun} and ${adjective} ${noun}. The aroma of ${adjective} popcorn wafted through the air, creating a ${adjective} ambiance fit for a ${adjective} tale. As the ${noun} dimmed, the ${adjective} world of ${character} unfolded on the big ${noun}, transporting us into a ${noun} where ${noun} and ${noun} collided in a ${adjective} dance. With each ${adjective} twist and ${noun}, I couldn't help but be $[verb] away by the ${adjective} spectacle, leaving the ${noun} with a ${noun} full of ${noun} and a newfound appreciation for the ${noun} that is a ${character} adventure on the ${adjective} ${noun}.`;
+let story = `Once upon a time, during a ${adjective} and ${color} afternoon, I ${adverb} went on a 
+${adjective} journey to the theaters to watch the latest Barbie movie titled "Barbie ${adjective} ${noun}." Excitement bubbled within me as I ${verb} into my ${adjective} seat, surrounded by a ${noun} of ${verb} ${noun} adorned in ${noun} and ${adjective} ${noun}. The aroma of ${adjective} popcorn wafted through the air, creating a ${adjective} ambiance fit for a ${adjective} tale. As the ${noun} dimmed, the ${adjective} world of ${character} unfolded on the big ${noun}, transporting us into a ${noun} where ${noun} and ${noun} collided in a ${adjective} dance With each ${adjective} twist and ${noun}, I couldn't help but be ${verb} away by the ${adjective} spectacle, leaving the ${noun} with a ${noun} full of ${noun} and a newfound appreciation for the ${noun} that is a ${character} adventure on the ${adjective} ${noun}.`;
 
 let madLibOutputDiv = document.getElementById("madLibOutput");
 
